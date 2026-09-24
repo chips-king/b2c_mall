340d306e7e086761a823911ba901ed947866a633
chips-king <211110790+chips-king@users.noreply.github.com>
2026-09-24 08:46:16 +0800

    order / SkuStockCheckCmp.java: Check product stock and listing status before payment

---
+++ order/src/main/java/com/b2cmall/order/liteflowcmp/SkuStockCheckCmp.java
@@ -0,0 +1,16 @@
+package com.b2cmall.order.liteflowcmp;
+
+import com.b2cmall.order.context.PaymentContext;
+import com.b2cmall.order.service.PaymentService;
+import com.yomahub.liteflow.annotation.LiteflowComponent;
+import com.yomahub.liteflow.core.NodeComponent;
+
+@LiteflowComponent("stockCheck")
+public class SkuStockCheckCmp extends NodeComponent {
+    private final PaymentService payments;
+    public SkuStockCheckCmp(PaymentService payments) { this.payments = payments; }
+    @Override public void process() {
+        PaymentContext context = getContextBean(PaymentContext.class);
+        payments.checkStock(context);
+    }
+}
